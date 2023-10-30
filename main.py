5e902d70c45f9605058907d503cb5b4d25323d7d
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-30 12:59:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -108,3 +108,200 @@ class Involution(keras.layers.Layer):
 
         # Return the output tensor and the kernel.
         return output, kernel
+
+# Define the input tensor.
+input_tensor = tf.random.normal((32, 256, 256, 3))
+
+# Compute involution with stride 1.
+output_tensor, _ = Involution(
+    channel=3, group_number=1, kernel_size=5, stride=1, reduction_ratio=1, name="inv_1"
+)(input_tensor)
+print(f"with stride 1 ouput shape: {output_tensor.shape}")
+
+# Compute involution with stride 2.
+output_tensor, _ = Involution(
+    channel=3, group_number=1, kernel_size=5, stride=2, reduction_ratio=1, name="inv_2"
+)(input_tensor)
+print(f"with stride 2 ouput shape: {output_tensor.shape}")
+
+# Compute involution with stride 1, channel 16 and reduction ratio 2.
+output_tensor, _ = Involution(
+    channel=16, group_number=1, kernel_size=5, stride=1, reduction_ratio=2, name="inv_3"
+)(input_tensor)
+print(
+    "with channel 16 and reduction ratio 2 ouput shape: {}".format(output_tensor.shape)
+
+# Load the CIFAR10 dataset.
+print("loading the CIFAR10 dataset...")
+(train_images, train_labels), (
+    test_images,
+    test_labels,
+) = keras.datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1.
+(train_images, test_images) = (train_images / 255.0, test_images / 255.0)
+
+# Shuffle and batch the dataset.
+train_ds = (
+    tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+    .shuffle(256)
+    .batch(256)
+)
+test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(256)
+
+class_names = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+]
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
+
+# Build the conv model.
+print("building the convolution model...")
+conv_model = keras.Sequential(
+    [
+        keras.layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3), padding="same"),
+        keras.layers.ReLU(name="relu1"),
+        keras.layers.MaxPooling2D((2, 2)),
+        keras.layers.Conv2D(64, (3, 3), padding="same"),
+        keras.layers.ReLU(name="relu2"),
+        keras.layers.MaxPooling2D((2, 2)),
+        keras.layers.Conv2D(64, (3, 3), padding="same"),
+        keras.layers.ReLU(name="relu3"),
+        keras.layers.Flatten(),
+        keras.layers.Dense(64, activation="relu"),
+        keras.layers.Dense(10),
+    ]
+)
+
+# Compile the mode with the necessary loss function and optimizer.
+print("compiling the convolution model...")
+conv_model.compile(
+    optimizer="adam",
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+# Train the model.
+print("conv model training...")
+conv_hist = conv_model.fit(train_ds, epochs=20, validation_data=test_ds)
+
+# Build the involution model.
+print("building the involution model...")
+
+inputs = keras.Input(shape=(32, 32, 3))
+x, _ = Involution(
+    channel=3, group_number=1, kernel_size=3, stride=1, reduction_ratio=2, name="inv_1"
+)(inputs)
+x = keras.layers.ReLU()(x)
+x = keras.layers.MaxPooling2D((2, 2))(x)
+x, _ = Involution(
+    channel=3, group_number=1, kernel_size=3, stride=1, reduction_ratio=2, name="inv_2"
+)(x)
+x = keras.layers.ReLU()(x)
+x = keras.layers.MaxPooling2D((2, 2))(x)
+x, _ = Involution(
+    channel=3, group_number=1, kernel_size=3, stride=1, reduction_ratio=2, name="inv_3"
+)(x)
+x = keras.layers.ReLU()(x)
+x = keras.layers.Flatten()(x)
+x = keras.layers.Dense(64, activation="relu")(x)
+outputs = keras.layers.Dense(10)(x)
+
+inv_model = keras.Model(inputs=[inputs], outputs=[outputs], name="inv_model")
+
+# Compile the mode with the necessary loss function and optimizer.
+print("compiling the involution model...")
+inv_model.compile(
+    optimizer="adam",
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+# train the model
+print("inv model training...")
+inv_hist = inv_model.fit(train_ds, epochs=20, validation_data=test_ds)
+
+
+conv_model.summary()
+
+inv_model.summary()
+
+
+plt.figure(figsize=(20, 5))
+
+plt.subplot(1, 2, 1)
+plt.title("Convolution Loss")
+plt.plot(conv_hist.history["loss"], label="loss")
+plt.plot(conv_hist.history["val_loss"], label="val_loss")
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.title("Involution Loss")
+plt.plot(inv_hist.history["loss"], label="loss")
+plt.plot(inv_hist.history["val_loss"], label="val_loss")
+plt.legend()
+
+plt.show()
+
+plt.figure(figsize=(20, 5))
+
+plt.subplot(1, 2, 1)
+plt.title("Convolution Accuracy")
+plt.plot(conv_hist.history["accuracy"], label="accuracy")
+plt.plot(conv_hist.history["val_accuracy"], label="val_accuracy")
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.title("Involution Accuracy")
+plt.plot(inv_hist.history["accuracy"], label="accuracy")
+plt.plot(inv_hist.history["val_accuracy"], label="val_accuracy")
+plt.legend()
+
+plt.show()
+
+
+layer_names = ["inv_1", "inv_2", "inv_3"]
+outputs = [inv_model.get_layer(name).output for name in layer_names]
+vis_model = keras.Model(inv_model.input, outputs)
+
+fig, axes = plt.subplots(nrows=10, ncols=4, figsize=(10, 30))
+
+for ax, test_image in zip(axes, test_images[:10]):
+    (inv1_out, inv2_out, inv3_out) = vis_model.predict(test_image[None, ...])
+
+    _, inv1_kernel = inv1_out
+    _, inv2_kernel = inv2_out
+    _, inv3_kernel = inv3_out
+
+    inv1_kernel = tf.reduce_sum(inv1_kernel, axis=[-1, -2, -3])
+    inv2_kernel = tf.reduce_sum(inv2_kernel, axis=[-1, -2, -3])
+    inv3_kernel = tf.reduce_sum(inv3_kernel, axis=[-1, -2, -3])
+
+    ax[0].imshow(keras.utils.array_to_img(test_image))
+    ax[0].set_title("Input Image")
+
+    ax[1].imshow(keras.utils.array_to_img(inv1_kernel[0, ..., None]))
+    ax[1].set_title("Involution Kernel 1")
+
+    ax[2].imshow(keras.utils.array_to_img(inv2_kernel[0, ..., None]))
+    ax[2].set_title("Involution Kernel 2")
+
+    ax[3].imshow(keras.utils.array_to_img(inv3_kernel[0, ..., None]))
+    ax[3].set_title("Involution Kernel 3")
